39cc8b34c04679076f137eaf1c213a054abea782
Fernando Illan <Fernando.Illan@syss.tech>
2021-03-06 17:33:15 -0600

    Ignore images

--- register_user.php
+++ register_user.php
@@ -10,15 +10,18 @@
     $password1 = $_POST['password1'];
     $sexID = $_POST['sexID'];
 
-    $adminsEmails = $adminUsers->getEmailAdmins();
-    $adminsEmails = implode(",", $adminsEmails);
-
-    if (strcmp($password, $password1) == 0) {
-        $password1 = sha1($password);
-        if ($adminUsers->addUser($firstname, $lastname, $email, $password, $sexID)) {
-            echo '<script>alert("User has been registered. We\'ll contact you by email.");</script>';
-            header('Location: index.php');
-        } else {
-            echo '<script>alert("Error");</script>';
+    if ($adminUsers->existEmail($_POST['email'])) {
+        echo '<script>alert("Error, there\'s an user with that email.");</script>';
+        header('Location: register.php');
+    } else {
+        if (strcmp($password, $password1) == 0) {
+            $password = sha1($password);
+            if ($adminUsers->addUser($firstname, $lastname, $email, $password, $sexID)) {
+                echo '<script>alert("User has been registered.");</script>';
+                header('Location: login.php');
+            } else {
+                echo '<script>alert("Error");</script>';
+            }
         }
-    }
+    }        
+    
